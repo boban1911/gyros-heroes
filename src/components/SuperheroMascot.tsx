3251b70782880ba6e772fe22e05e3a89a6bb4f59
bdjordjevic <b.djordjevic@netconomy.net>
2026-02-19 15:51:57 +0100

    style(ui): Adjust mascot size and positioning to match Figma visual reference

--- src/components/SuperheroMascot.tsx
+++ src/components/SuperheroMascot.tsx
@@ -4,7 +4,7 @@ import superheroTortilla from '../assets/superhero-tortilla.svg';
 const SuperheroMascot: React.FC = () => {
   return (
     <div 
-      className="absolute left-4 md:left-10 lg:left-[20px] bottom-[-100px] md:bottom-[-150px] lg:bottom-[-200px] z-10 hidden md:block w-48 md:w-64 lg:w-[450px]"
+      className="absolute left-4 md:left-10 lg:left-[20px] bottom-[-80px] md:bottom-[-120px] lg:bottom-[-180px] z-10 hidden md:block w-40 md:w-56 lg:w-[350px]"
       data-testid="superhero-mascot"
     >
       <img 
